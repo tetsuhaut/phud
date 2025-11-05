238875f5739a651d625c0eff715228dbbcebf0b2
Tetsuhaut <sjoucreau@gmail.com>
2025-11-05 13:25:43 +0100

    wip

---
+++ build-clang.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+REM no SETLOCAL to stay in the build dir so that "make" works
+where /q ninja || ECHO Could not find the ninja command. Check that it is in the path. && EXIT /B 1
+REM SET CMAKE_GENERATOR="Eclipse CDT4 - MinGW Makefiles"
+SET CMAKE_GENERATOR="Ninja"
+SET BUILD_DIR=build
+SET BIN_DIR=bin\Debug
+CALL build.bat
+@ECHO OFF
+IF ERRORLEVEL 1 ECHO Error building the phud Makefile. && EXIT /B 1
+REM ECHO ^color ^&^& ^cls ^&^& ^mingw32-make -j 4 %%* > %BUILD_DIR%\make.bat
+REM ECHO ^color ^&^& ^cls ^&^& ^^mingw32-make -j 4 ^&^& test %%* > %BUILD_DIR%\makeTest.bat
+ECHO ^color ^&^& ^cls ^&^& ^ninja %%* > %BUILD_DIR%\make.bat
+ECHO ^color ^&^& ^cls ^&^& ^ninja ^&^& %BIN_DIR%\unitTests.exe %%* > %BUILD_DIR%\makeTest.bat
+PUSHD %BUILD_DIR%
+ENDLOCAL
+@ECHO ON
