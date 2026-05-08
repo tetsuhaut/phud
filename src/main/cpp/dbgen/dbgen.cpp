e6a51f32d181abe8a0bae74007b394911f52f912
Tetsuhaut <sjoucreau@gmail.com>
2026-05-08 18:06:50 +0200

    wip

--- src/main/cpp/dbgen/dbgen.cpp
+++ src/main/cpp/dbgen/dbgen.cpp
@@ -3,21 +3,15 @@
 #include "entities/Site.hpp"
 #include "filesystem/FileUtils.hpp"     // phud::filesystem::*
 #include "history/PokerSiteHistory.hpp" // std::filesystem::path
+#include "log/Logger.hpp" // Logger::*
 #include "language/limits.hpp"          // toSizeT
-#include "log/Logger.hpp"               // CURRENT_FILE_NAME
 #include "strings/StringUtils.hpp"
 #include <optional>
 #include <print>
 #include <utility> // std::pair
 
-namespace ps = phud::strings;
-
-static Logger& LOG() {
-  static auto logger = Logger(CURRENT_FILE_NAME);
-  return logger;
-}
-
 namespace fs = std::filesystem;
+namespace ps = phud::strings;
 
 namespace {
   struct [[nodiscard]] MyLoggingConfig final {
@@ -31,12 +25,10 @@ namespace {
 
   [[nodiscard]] std::optional<fs::path>
   getOptionalDbHistory(std::span<const char* const> args) {
+    const auto programName = std::string_view(args[0]);
     const auto askForHelp = (1 == args.size()) or std::ranges::any_of(args, [](auto arg) {
       return nonCaseSentitiveEquals(arg, "-h") or nonCaseSentitiveEquals(arg, "--help");
     });
-
-    const auto programName = std::string_view(args[0]);
-
     if (askForHelp) {
       std::println("Generates the Poker Head Up Display database.");
       std::println("{} -d <history directory>", programName);
@@ -47,13 +39,11 @@ namespace {
       std::println(stderr, "{} -d <history directory>", programName);
       return {};
     }
-
-    const auto flag = std::string_view(args[1]);
     const auto historyDir = fs::path(args[2]);
 
-    if (!PokerSiteHistory::isValidHistory(historyDir)) {
-      LOG().error<"'{}' is not a valid history directory">(historyDir.string());
-      LOG().error<"{} -d <history directory>\n">(programName);
+    if (!phud::filesystem::isDir(historyDir)  or !PokerSiteHistory::isValidHistory(historyDir)) {
+      std::println(stderr, "'{}' is not a valid history directory", historyDir.string());
+      std::println(stderr, "{} -d <history directory>", programName);
       return {};
     }
 
@@ -63,14 +53,14 @@ namespace {
 
 int main(int argc, const char* const argv[]) {
   std::setlocale(LC_ALL, "en_US.utf8");
-  MyLoggingConfig _;
+  MyLoggingConfig _; // will be used in the PokerSiteHistory and Database objects
 
 #ifdef __clang__
 #  pragma clang diagnostic push
 #  pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
 #endif
 
-  const std::span args = {argv, argv + argc};
+  const auto args = std::span(argv, argv + argc);
 
 #ifdef __clang__
 #  pragma clang diagnostic pop
